0676b7c26c61f6978331c1e005f1da768e625851
luciano-drozda <drozda.luciano@gmail.com>
2026-08-21 23:56:08 +0200

    Delete legacy 'src/'

--- src/val-corpus-keep_push_pop_false-site_level_tbr_true/quadloss.jl
+++
@@ -1,3 +0,0 @@
-function quadloss(loss, x, y, z)
-    loss[1] = (x ^ 2 * y - 3.0 * y * z) + z ^ 3
-end
--- src/val-corpus-keep_push_pop_false/dotprod.jl
+++
@@ -1,5 +0,0 @@
-function dotprod(loss, u, v, i_n)
-    for i_seq_x = 1:i_n
-        loss[1] = loss[1] + u[i_seq_x] * v[i_seq_x]
-    end
-end
