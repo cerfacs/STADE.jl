8f99d5e7bd327aeb9d8e3aac14833c91c1734c32
luciano-drozda <drozda.luciano@gmail.com>
2026-08-23 10:22:38 +0000

    Update

--- gallery/affine_loss.jl
+++ gallery/affine_loss.jl
@@ -1,3 +1,14 @@
+# affine_loss(loss, u, a, b, v, i_n)
+#
+# An elementwise affine map v = a*u + b, followed by a sum-of-squares
+# loss over v.
+#
+# loss: length-1 output array; loss[1] receives the sum of v[i]^2
+# u: input field, length i_n
+# a: elementwise scale, length i_n
+# b: elementwise shift, length i_n
+# v: scratch/output array for a*u + b, length i_n
+# i_n: number of elements
 function affine_loss(loss, u, a, b, v, i_n)
     for i_x = 1:i_n
         v[i_x] = a[i_x] * u[i_x] + b[i_x]
@@ -5,4 +16,5 @@ function affine_loss(loss, u, a, b, v, i_n)
     for i_seq_x = 1:i_n
         loss[1] = loss[1] + v[i_seq_x] ^ 2
     end
+    return nothing
 end
